0f9786cd58303c056ed471658cb40faa5c789290
manchuriqbal <manchur.fiverr@gmail.com>
2023-10-08 22:51:27 +0600

    i complete resume project

--- pdf/urls.py
+++ pdf/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='pdf')
+    path('', views.index, name='pdf'),
+    path('<int:pk>/', views.resume, name='resume'),
+    path('list/', views.list, name='list')
 ]
--- pdf/views.py
+++ pdf/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+import pdfkit
+import io
 
 from pdf.models import Profile
 
@@ -17,4 +22,23 @@ def index(request):
         profil = Profile(name=name, phone=phone, email=email, school=school, degree=degree, university=university, skill=skill, about_you=about_you, previous_work=previous_work)
         profil.save()
 
-    return render(request, 'pdf/index.html')
+    return render(request, 'templates/pdf/index.html')
+
+
+def resume(request, pk):
+    user_profile = Profile.objects.get(pk=pk)
+    template = loader.get_template('templates/pdf/resume.html')
+    html = template.render({'user_profile':user_profile})
+    option = {
+        'page-size' : 'Letter',
+        'encoding' : 'UTF-8'
+    }
+    pdf_config = pdfkit.configuration(wkhtmltopdf='C:\Program Files\wkhtmltopdf\\bin\wkhtmltopdf.exe')
+    pdf = pdfkit.from_string(html, False, option, configuration=pdf_config)
+    response = HttpResponse(pdf,content_type='application/pdf')
+    response['Content-Dispositon'] = 'attachments'
+    return response
+
+def list(request):
+    profile = Profile.objects.all()
+    return render(request, 'templates/pdf/list.html', {'profile': profile})
